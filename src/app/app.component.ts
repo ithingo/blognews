0aff133c15240c2ebe74eaf60df0ed3e147a5b82
Vladimir Borodaev <v.borodaev@dunice.net>
2018-07-24 15:29:55 +0300

    Redesigned news fromat with stylization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 // Temporary interface, later - separated component
+interface Person {
+  imageUrl: string;
+  fullName: string;
+}
+
 interface News {
-  newsTitle: string;
-  imageLink: string;
+  subject: string;
   content: string;
+  person: Person;
   // [hashtag: string]: string;
 }
 
@@ -19,39 +24,42 @@ export class AppComponent {
   textBeforeNews = 'What is new at the moment:';
   _itemNewsArray: News[] = [
     {
-      'newsTitle': 'Breaking news: Hillary - President!',
-      'imageLink': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Hillary_Clinton_Testimony_to_House_Select_Committee_on_Benghazi_%281%29.png/200px-Hillary_Clinton_Testimony_to_House_Select_Committee_on_Benghazi_%281%29.png',
-      'content': 'Hillary Clinton became the president of independent California Republic',
+      'subject': 'I am leading!',
+      'content': `Finally we won! Thanks to all people that have voted for me this election.
+        Although I cannot make all Americans free from that Beast, I swear the people of
+        Independent California Republic will be free forever!`,
+      'person': {
+        'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Hillary_Clinton_Testimony_to_House_Select_Committee_on_Benghazi_%281%29.png/200px-Hillary_Clinton_Testimony_to_House_Select_Committee_on_Benghazi_%281%29.png',
+        'fullName': 'Hillary Clinton',
+      }
     },
     {
-      'newsTitle': 'Make North Korea great again!',
-      'imageLink': 'http://www.ddinews.gov.in/sites/default/files/kimoo.jpg',
-      'content': 'Kim Jong Un was awarded as the main peace-maker',
+      'subject': 'Make America Great Forever!',
+      'content': `Crooked Hillary wins again... She will destroy the California like she has destroyed the US.
+        But I swear to all people that I will make the America great whatever trouble may come`,
+      'person': {
+        'imageUrl': 'https://ly.usembassy.gov/wp-content/uploads/sites/246/16114538_1227645557304882_4439457132240667293_n-1.jpg',
+        'fullName': 'Donald J. Trump',
+      }
     },
     {
-      'newsTitle': `To "Space" and further`,
-      'imageLink': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Elon_Musk_2015.jpg/267px-Elon_Musk_2015.jpg',
-      'content': 'Elon Musk send a thousandth car into space',
+      'subject': 'Lord of the Light warnings',
+      'content': `We, the servants of the Lord of the Light, warns you that difficult times is to come.
+        We should all prepare for the Long Night. For the night is dark and full of terrors`,
+      'person': {
+        'imageUrl': 'https://www.telegraph.co.uk/content/dam/tv/2016/04/25/melisandre_trans_NvBQzQNjv4BqutubNGxeqbD0m2XylzINLiOoem_3qpp9C-iKHR23jxY.jpg?imwidth=1400',
+        'fullName': 'Melisandre',
+      }
     },
     {
-      'newsTitle': `I'll shake the universe!`,
-      'imageLink': 'https://vignette.wikia.nocookie.net/fictional-battle-omniverse/images/e/e0/Hades_Disney.png/revision/latest/scale-to-width-down/300?cb=20171108000219',
-      'content': 'Hades became tha main Olympic god instead of Zeus',
-    },
-    {
-      'newsTitle': `VB lives forever`,
-      'imageLink': 'https://static.memrise.com/img/400sqf/from/uploads/course_photos/3146044000151118233245.jpg',
-      'content': 'Visual Basic drops down the rating of C/C++ and Java',
-    },
-    {
-      'newsTitle': `Bill Gates loves Apple`,
-      'imageLink': 'https://news.microsoft.com/uploads/2017/03/gates_page.png',
-      'content': 'Bill Gates became the CEO of Apple, Inc.',
-    },
-    {
-      'newsTitle': `Train your memory`,
-      'imageLink': 'https://getlighthouse.com/blog/wp-content/uploads/2015/04/Screen-Shot-2015-04-29-at-11.46.50-PM.png',
-      'content': 'I lost all my stickers, so I remember nothing',
+      'subject': 'Winter is coming',
+      'content': `To all lords of Seven Kingdoms. Winter is coming!
+        All people should prepare for the Long Night. We need more man in the Night Watch!
+        Please send whoever you have: bastards, prisoners etc. Jon Snow, Lord Commander of the Night Watch`,
+      'person': {
+        'imageUrl': 'https://pmctvline2.files.wordpress.com/2016/02/game-of-thrones-jon-snow.jpg',
+        'fullName': 'Jon Snow',
+      }
     },
   ];
 
@@ -75,7 +83,8 @@ export class AppComponent {
   filterByKeywords(news: News, keywords: string[]): News {
     let isFounded = false;
     keywords.forEach(key => {
-      if (news.newsTitle.includes(key) || news.content.includes(key)) {
+      if (news.subject.toLowerCase().includes(key.toLowerCase())
+        || news.content.toLowerCase().includes(key.toLowerCase())) {
         isFounded = true;
       }
 
