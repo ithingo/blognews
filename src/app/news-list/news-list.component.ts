c53ca0f1abab6e7376f77c47619f87ff8b6495b6
Vladimir Borodaev <v.borodaev@dunice.net>
2018-07-26 12:19:28 +0300

    Moved temp (mock) data to separate file, preparing to use services

--- src/app/news-list/news-list.component.ts
+++ src/app/news-list/news-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { News } from '../news/news';
 import { SelectionProperties } from '../search-selection/selection-properties';
 
+import { NEWSARRAY } from '../mock-news-lis'; // move all temp data as mock to separate file
+
 @Component({
   selector: 'app-news-list',
   templateUrl: './news-list.component.html',
@@ -13,55 +15,7 @@ export class NewsListComponent implements OnInit {
 
   _optionToFilter: SelectionProperties = null;
 
-  _itemNewsArray: News[] = [
-    {
-      subject: 'I am leading!',
-      content: `Finally we won! Thanks to all people that have voted for me this election.
-        Although I cannot make all Americans free from that Beast, I swear the people of
-        Independent California Republic will be free forever!`,
-      person: {
-        'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Hillary_Clinton_Testimony_to_House_Select_Committee_on_Benghazi_%281%29.png/200px-Hillary_Clinton_Testimony_to_House_Select_Committee_on_Benghazi_%281%29.png',
-        'fullName': 'Hillary Clinton',
-      }
-    },
-    {
-      subject: 'Make America Great Forever!',
-      content: `Crooked Hillary wins again... She will destroy the California like she has destroyed the US.
-        But I swear to all people that I will make the America great whatever trouble may come`,
-      person: {
-        'imageUrl': 'https://ly.usembassy.gov/wp-content/uploads/sites/246/16114538_1227645557304882_4439457132240667293_n-1.jpg',
-        'fullName': 'Donald J. Trump',
-      }
-    },
-    {
-      subject: 'Lord of the Light warnings',
-      content: `We, the servants of the Lord of the Light, warns you that difficult times is to come.
-        We should all prepare for the Long Night. For the night is dark and full of terrors`,
-      person: {
-        'imageUrl': 'https://www.telegraph.co.uk/content/dam/tv/2016/04/25/melisandre_trans_NvBQzQNjv4BqutubNGxeqbD0m2XylzINLiOoem_3qpp9C-iKHR23jxY.jpg?imwidth=1400',
-        'fullName': 'Melisandre',
-      }
-    },
-    {
-      subject: 'Winter is coming',
-      content: `To all lords of Seven Kingdoms. Winter is coming!
-        All people should prepare for the Long Night. We need more man in the Night Watch!
-        Please send whoever you have: bastards, prisoners etc. Jon Snow, Lord Commander of the Night Watch`,
-      person: {
-        'imageUrl': 'https://pmctvline2.files.wordpress.com/2016/02/game-of-thrones-jon-snow.jpg',
-        'fullName': 'Jon Snow',
-      }
-    },
-    {
-      subject: 'I am tired',
-      content: `I am tired, I leave my boring kingdom.... I want more: I want to rule the See, like Poseidon, or the Heavens, like Zeus.
-        I hate all these shadows in this dark place. Al right, I made a decision... But I should borrow my Fluffy with me....`,
-      person: {
-        'imageUrl': 'https://ohmy.disney.com/wp-content/uploads/2014/05/Reasons-We-Cant-Help-but-Love-Hades-Fiery.png',
-        'fullName': 'Hades',
-      }
-    },
-  ];
+  _itemNewsArray: News[] = NEWSARRAY;
 
   // Array filtered by input text's key words
   _filteredArray: Array<News> = [];
