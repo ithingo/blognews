a2795fe23a29872cd36e3a6a3576de15a85f7b32
Vladimir Borodaev <v.borodaev@dunice.net>
2018-07-26 10:45:15 +0300

    Added template navigation to user-profile, without passing params. works)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NewsComponent } from './news/news.component';
 import { SearchSelectionComponent } from './search-selection/search-selection.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'profile',
+    component: UserProfileComponent
+  },
+  {
+    path: 'all-feeds',
+    component: AppComponent
+  },
+  {
+    path: '',
+    redirectTo: 'all-feeds',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
     SearchSelectionComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      // temp for tracing in debug mode
+      {enableTracing: true},
+    ),
     BrowserModule,
     FormsModule,
   ],
