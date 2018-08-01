de881f94b7de40e73eb045d8ea5638d92001e6a5
Vladimir Borodaev <v.borodaev@dunice.net>
2018-08-01 15:06:57 +0300

    Temp commit, partially created custom model form, implemented service for the form

--- src/app/user/edit-profile-window/edit-profile-window.component.ts
+++ src/app/user/edit-profile-window/edit-profile-window.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, Output, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { EventEmitter } from 'events';
 
 import { EditProfileWindowService } from '../../edit-profile-window.service';
 import { UserType } from '../user-type';
@@ -6,11 +8,25 @@ import { UserType } from '../user-type';
 @Component({
   selector: 'app-edit-profile-window',
   templateUrl: './edit-profile-window.component.html',
-  styleUrls: ['./edit-profile-window.component.css']
+  styleUrls: ['./edit-profile-window.component.css'],
+  animations: [
+    trigger('dialog', [
+      transition('void => *', [
+        style({ transform: 'scale3d(0.3, 0.3, 0.3)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'scale3d(0.0, 0.0, 0.0)' }))
+      ])
+    ])
+  ]
 })
 export class EditProfileWindowComponent implements OnInit {
-  @Input() id: string;
-  @Input() user: UserType = null;
+  @Input() closable = true;
+  @Input() visible: boolean = false;
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  private user: UserType;  // there should be user after retreaving via service from cookies
 
   // private user_first_name: string;
   // private user_second_name: string;
@@ -26,7 +42,12 @@ export class EditProfileWindowComponent implements OnInit {
   private user_name: string;
   private user_photo: string;
 
-  constructor(private _editModalService: EditProfileWindowService) {
+  constructor(
+    private _editModalService: EditProfileWindowService,
+    private _element: ElementRef
+  ) {
+    this._element = _element.nativeElement;
+
     // for temp mock data
     this.user_id = this.user.id;
     this.user_fullName = this.user.fullName;
@@ -37,10 +58,20 @@ export class EditProfileWindowComponent implements OnInit {
   }
 
   ngOnInit() {
+    const modal = this;
+
+
+    this._editModalService.add(modal)
   }
 
-  closeModal(id: string) {
-    this._editModalService.close(id);
+  open() {
+    this.visible = true;
+    this.visibleChange.emit(this.visible);
+  }
+
+  close() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
   }
 
 }
