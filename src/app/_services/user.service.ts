046ec7b5972c58ddf280a792b2f293531023e3c0
Vlad Borodaev <nikoskefali@gmail.com>
2018-08-05 19:57:41 +0300

    Removed unused parts of code with mock data

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -4,7 +4,6 @@ import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { UserType } from '../user/user-type';
-// import { USERARRAY } from '../mock-data';
 
 @Injectable({
   providedIn: 'root'
@@ -14,24 +13,7 @@ export class UserService {
 
   constructor(public http: HttpClient) { }
 
-  // getNews(): Observable<UserType[]> {
-    // const apiRoot = `${this.host}/api/v1/posts/`;
-    // const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6InRlc3RAdGVzdC5ydSIsImV4cCI6MTUzMzQ1MDk3NSwiZW1haWwiOiJ0ZXN0QHRlc3QucnUifQ.7D77tK72RjT7OWlwJWv9IOzeoHshwaXHfx7FwxT8utU';
-
-    // const headers = new HttpHeaders();
-    // headers.append('Content-Type', 'application/json');
-    // headers.append('Authorization', `JWT ${token}`);
-
-    // return this.http
-    //   .get<UserType[]>(apiRoot, {headers: headers})
-  // }
-
-  // getAllUsers(): Observable<UserType[]> {
-  //   return of(USERARRAY);     // real http -> HttpClient.get<UserType[]>
-  // }
-
   getUserById(user_id: number): Observable<UserType> {
-    // return of(USERARRAY.find(user => user.id === id));
     const apiRoot = `${this.host}/api/v1/users/${user_id}`;
     const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6InRlc3RAdGVzdC5ydSIsImV4cCI6MTUzMzQ1MDk3NSwiZW1haWwiOiJ0ZXN0QHRlc3QucnUifQ.7D77tK72RjT7OWlwJWv9IOzeoHshwaXHfx7FwxT8utU';
 
@@ -42,6 +24,4 @@ export class UserService {
     return this.http
       .get<UserType>(apiRoot, {headers: headers})
   }
-
-
 }
